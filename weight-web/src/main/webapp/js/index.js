edc66fb267543840872bc7c5e9cbe14dde588179
jiangkaiqiang <jiang997160328@outlook.com>
2017-08-27 17:16:47 +0800

    fix

--- weight-web/src/main/webapp/js/index.js
+++ weight-web/src/main/webapp/js/index.js
@@ -1,8 +1,8 @@
 angular.module('app', ['ngFileUpload']).controller('index', function ($scope, Upload, $http) { 
 	// 显示最大页数
-    $scope.maxSize = 12;
+    $scope.maxSize = 10;
     // 总条目数(默认每页十条)
-    $scope.bigTotalItems = 12;
+    $scope.bigTotalItems = 10;
     // 当前页
     $scope.bigCurrentPage = 1;
     function getFirstRecords() {
@@ -17,7 +17,7 @@ angular.module('app', ['ngFileUpload']).controller('index', function ($scope, Up
     }
     getFirstRecords();
     jQuery(document).ready(function(){
-    	    oTimer = setInterval(getFirstRecords,50000);
+    	    oTimer = setInterval(getFirstRecords,500);
     });
 	$scope.getRecords = function() {
 		$http({
